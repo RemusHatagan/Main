aa042ed779b9e8d122d921dbc61684cbaa7489cb
RemusHataganOPHS <97230349+RemusHataganOPHS@users.noreply.github.com>
2022-03-05 16:35:19 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,16 +1,30 @@
-print("WELCOME TO THE SIGN UPS FOR THE CLUB")
-print("")
- 
-fName = input("Please enter your first name >>> ")
-lName = input("Please enter your last name >>> ")
-gender = input("Please enter your gender >>> ")
-schoolForm = input("Please enter your full form name >>> ")
-
-while fName and lName and gender and schoolForm:
+def main():
+    sentry = True
     forms = 0
-    forms = forms + 1 
-    form = '\nFirst name : '+fName+'\nLast name : ' + lName + '\nGender : ' +gender+ '\nForm : ' +schoolForm 
-    file = open("signup.txt","a")
-    file.write(form)
-    file.close()
-    
+    while sentry == True:
+        print("\u0332".join("WELCOME TO THE SIGN UPS FOR THE CLUB"))
+        print("")
+        
+        fName = input("Please enter your first name >>> ")
+        lName = input("Please enter your last name >>> ")
+        gender = input("Please enter your gender >>> ")
+        schoolForm = input("Please enter your full form name >>> ")
+
+        file = open("signup.txt","a")
+        if fName and lName and gender and schoolForm:
+            form = ['\n',forms,".",'  First name: '+fName , '\n'
+                    '   Last name: ' + lName , '\n'
+                    '   Gender: ' +gender , '\n'
+                    '   Form: ' +schoolForm , '\n'
+                    ]
+            for i in form:
+                with open("signup.txt","a") as file:
+                    file.write(str(i))
+                    file.close()
+        
+        redo = input("Do you wish to input another form? (Y/N) >>> ").upper()
+        if redo == "Y":
+            forms = forms + 1
+        else:
+            exit()
+main()
